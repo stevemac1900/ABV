54ee1f0e412b9c30fa011f86bb66f1074dae2d86
Jose Meono <stjam26@moravian.edu>
2018-02-21 17:22:52 -0500

    Implement logging and go function

--- src/beerapi.py
+++ src/beerapi.py
@@ -2,17 +2,28 @@ import requests
 import datetime
 import time
 
+
+import logging
+
+logging.basicConfig(filename= 'test.log',level=logging.INFO,format='%(asctime)s:%(message)s')
+
+
+
 def get_beer_data():
     beer_inventory = requests.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv')
     filename = str(datetime.datetime.now()).replace(' ', '_') + '.csv'
     beer_inventory_file = open(filename, "w+")
     beer_inventory_file.write(beer_inventory.text)
-    time.sleep(3600)
 
-while True:
-    try:
-        get_beer_data()
-        time.sleep(3600)
-    except:
-        time.sleep(3600)
-        continue
+
+def go():
+
+    while True:
+        try:
+            get_beer_data()
+            logging.info('The data was fetched successfully!')
+            time.sleep(1200)
+        except:
+            logging.exception('Failed to fetch file:')
+            time.sleep(1200)
+            pass
