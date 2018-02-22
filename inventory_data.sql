0ce50d7d8b8f77faf4e8f0b7e56dc8221868a187
Steve MacDonald <stsjm10@moravian.edu>
2018-02-22 09:08:59 -0500

    Adds datetime field and indexing, splits size field into three
    Adding a datetime field necessitated two tables, one for static information and
    one for variable. It also means we will need indexing since we will be storing
    significantly more data.
    
    The size field, upon further inspection, actually holds the information for 2 or
    3 fields depending whether the entry is for kegs or cans/bottles. This has been
    split into 3 separate fields - containers_per_pack, container_size and
    container_type.
    
    Additionally the case_pack field has been renamed to better reflect the values
    being stored there.

--- inventory_data.sql
+++ inventory_data.sql
@@ -4,13 +4,24 @@ CREATE DATABASE inventory_data;
 
 USE inventory_data;
 
-CREATE TABLE Inventory (
-  id INT(3) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50),
-  size VARCHAR(20),
-  category VARCHAR(20),
-  quantity_available FLOAT UNSIGNED,
-  retail_price FLOAT UNSIGNED,
-  case_retail_price FLOAT UNSIGNED,
-  case_pack INT UNSIGNED;
+CREATE TABLE inventory(
+  beerID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(500),
+  containers_per_pack INT,
+  container_size VARCHAR(200),
+  container_type VARCHAR(100),
+  category VARCHAR(200),
+  packs_per_case INT;
 )
+
+CREATE TABLE purchasing_details(
+  dt DATETIME DEFAULT CURRENT_TIMESTAMP,
+  quantity_available FLOAT,
+  retail_price FLOAT,
+  case_retail_price FLOAT,
+  id INT NOT NULL FOREIGN KEY REFERENCES Inventory(beerID);
+)
+
+-- Rename fields
+CREATE INDEX AUTHOR_INDEX
+ON Inventory_availability(dt);
