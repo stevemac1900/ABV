77fea996342b6455f7fcbb5fbd91c2b98e83fc96
John Vonelli <stjpv03@moravian.edu>
2018-02-22 18:54:10 -0500

    Add documentation for file

--- src/beerapi.py
+++ src/beerapi.py
@@ -3,16 +3,24 @@ import datetime
 import time
 
 def get_beer_data():
+    """
+    Retrieves inventory data from Tanczos and saves data locally
+
+    Input:
+        Parameterless
+
+    Output:
+        Returns void and writes a csv file locally
+    """
     beer_inventory = requests.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv')
     filename = str(datetime.datetime.now()).replace(' ', '_') + '.csv'
     beer_inventory_file = open(filename, "w+")
     beer_inventory_file.write(beer_inventory.text)
-    time.sleep(3600)
 
 while True:
     try:
         get_beer_data()
-        time.sleep(3600)
     except:
-        time.sleep(3600)
-        continue
+        pass
+
+    time.sleep(3600)
