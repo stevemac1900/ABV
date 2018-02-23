1ab0853024b66f83a5fc924c0dc51559cc87c68f
Steve MacDonald <stsjm10@moravian.edu>
2018-02-23 14:18:49 -0500

    Fix documentation to accurately reflect which function raises exceptions

--- src/beerapi.py
+++ src/beerapi.py
@@ -3,6 +3,7 @@ import datetime
 import time
 import logging
 
+
 def write_beer_inventory():
     """
     Retrieves inventory data from Tanczos and writes to file locally
@@ -12,11 +13,16 @@ def write_beer_inventory():
 
     Output:
         Returns void
+
+    Raises:
+    Exception
+        When unable to fetch file, logs error and passes
     """
     beer_inventory = requests.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv')
     filename = str(datetime.datetime.now()).replace(' ', '_') + '.csv'
     with open(filename, "w+") as beer_inventory_file:
         beer_inventory_file.write(beer_inventory.text)
+
 
 def run():
     """
@@ -27,10 +33,6 @@ def run():
 
     Output:
         Returns void
-
-    Raises:
-        Exception
-            When unable to fetch file, logs error and passes
     """
 
     while True:
@@ -42,6 +44,7 @@ def run():
             logging.exception('Failed to fetch file:')
             pass
         time.sleep(1200)
+
 
 if __name__ == "__main__":
     logging.basicConfig(filename='test.log', level=logging.INFO, format='%(asctime)s:%(message)s')
