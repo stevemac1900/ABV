12001cb3d789aa0bdbd2f807066fbe9ac17c75ad
Steve MacDonald <stsjm10@moravian.edu>
2018-03-02 10:38:11 -0500

    Consolidate 2 except blocks into 1

--- src/beerapi.py
+++ src/beerapi.py
@@ -2,6 +2,8 @@ import requests
 import datetime
 import time
 import logging
+
+
 def write_beer_inventory():
     """
     Retrieves inventory data from Tanczos and writes to file locally
@@ -23,17 +25,12 @@ def write_beer_inventory():
             beer_inventory_file.write(beer_inventory.text)
         logging.info('The data was fetched successfully!')
 
-    except OSError as e:
+    except (OSError, requests.exceptions.ConnectionError) as e:
         error_subclass = type(e).__name__
         logging.exception('Failed to fetch file: {}'.format(error_subclass))
         pass
 
-    except requests.exceptions.ConnectionError as err:
-        error = str(err).split(':')
-        arrayLen = len(error)
-        error_msg = format(type(err).__name__) + " " + error[arrayLen - 2][1:] + ":" + error[arrayLen - 1][:-4] + ")"
-        logging.warning(error_msg)
-        pass
+
 def run():
     """
     Runs write_beer_inventory every 20 minutes
