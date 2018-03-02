985fc0a26b00e4471e1686ae90ff1296076d35e4
Steve MacDonald <stsjm10@moravian.edu>
2018-03-02 12:15:46 -0500

    Changes dt field so it no longer stores a timestamp, just datetime

---
+++ src/inventory_data.sql
@@ -0,0 +1,27 @@
+DROP DATABASE inventory_data;
+
+CREATE DATABASE inventory_data;
+
+USE inventory_data;
+
+CREATE TABLE inventory(
+  beerID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(500),
+  containers_per_pack INT,
+  container_size VARCHAR(200),
+  container_type VARCHAR(100),
+  category VARCHAR(200),
+  packs_per_case INT;
+)
+
+CREATE TABLE purchasing_details(
+  dt DATETIME DEFAULT,
+  quantity_available FLOAT,
+  retail_price FLOAT,
+  case_retail_price FLOAT,
+  id INT NOT NULL FOREIGN KEY REFERENCES Inventory(beerID);
+)
+
+-- Rename fields
+CREATE INDEX DATETIME_INDEX
+ON Inventory_availability(dt);
