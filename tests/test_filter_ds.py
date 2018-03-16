e77e715874b50716536550583f53f6e671c42769
John Vonelli <stjpv03@moravian.edu>
2018-03-16 15:16:13 -0400

    Update nomenclature of package to size within the class and test

--- tests/test_filter_ds.py
+++ tests/test_filter_ds.py
@@ -2,20 +2,20 @@ from FilterDS import FilterDS
 
 def test_filter_with_no_filters():
     filter = FilterDS()
-    assert None == filter.package
+    assert None == filter.size
     assert None == filter.style
 
 def test_filter_with_package_filter():
-    filter = FilterDS(package='keg')
-    assert 'keg' == filter.package
+    filter = FilterDS(size='keg')
+    assert 'keg' == filter.size
     assert None == filter.style
 
 def test_filter_with_style_filter():
     filter = FilterDS(style='porter')
-    assert None == filter.package
+    assert None == filter.size
     assert 'porter' == filter.style
 
 def test_filter_with_both_filters():
-    filter = FilterDS(package='keg', style='porter')
-    assert 'keg' == filter.package
+    filter = FilterDS(size='keg', style='porter')
+    assert 'keg' == filter.size
     assert 'porter' == filter.style
