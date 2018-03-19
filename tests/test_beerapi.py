de6b88b2cfbe673a182f4954bb702883ece8171f
Jose Meono <jose.a.meono@gmail.com>
2018-03-19 12:17:28 -0400

    Refactor beerapi.py and tests exceptions

--- tests/test_beerapi.py
+++ tests/test_beerapi.py
@@ -4,21 +4,25 @@ import requests_mock
 import src.beerapi as beerapi
 
 
-
-#TODO: Make this test of success work with the current code of beerapi somehow
-@pytest.fixture()
-def expected_success():
+@pytest.fixture(params= [requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError])
+def expected_exception_on_read(request):
     with requests_mock.Mocker() as session:
-        session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', text = 'success')
+        session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', exc=request.param)
+        yield request.param
+
+
+def test_connection_exceptions_on_read(expected_exception_on_read):
+    assert beerapi.get_inventory() is ""
+
+
+@pytest.fixture()
+def expected_exception_on_write():
+    with requests_mock.Mocker() as session:
+        session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', exc=OSError)
         yield
 
 
-@pytest.fixture(params= [requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError])
-def expected_exception(request):
-    with requests_mock.Mocker() as session:
-        session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', exc = request.param)
-        yield request.param
-
-def test_connection_exceptions(expected_exception):
-    with pytest.raises(expected_exception):
+def test_exception_on_write(expected_exception_on_write):
+    with pytest.raises(OSError):
         beerapi.write_beer_inventory();
+
