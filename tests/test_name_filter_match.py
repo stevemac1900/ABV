57f76e74adaa9fd12cee00ef639911ebd3e097ef
Mario <stmos01@moravian.edu>
2018-03-20 16:54:47 -0400

    Add test for no match

--- tests/test_name_filter_match.py
+++ tests/test_name_filter_match.py
@@ -6,3 +6,9 @@ def test_name_filter_match():
     b = Beer('bud', '1/2 keg', 'lager', '2.00')
     f = NameFilter(YesFilter(), 'bud')
     assert f.is_match(b) is True
+
+
+def test_name_filter_no_match():
+    b = Beer('bud', '1/2 keg', 'lager', '2.00')
+    f = NameFilter(YesFilter(), 'bud light')
+    assert f.is_match(b) is False
