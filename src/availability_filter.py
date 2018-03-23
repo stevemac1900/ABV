cc36f940a55e4b3c4f01f8d5a4997834643895ea
Steve MacDonald <stsjm10@moravian.edu>
2018-03-22 22:23:14 -0400

    Check availability by the quantity_available field, adds relevant testsx

--- src/availability_filter.py
+++ src/availability_filter.py
@@ -1,9 +1,9 @@
 class AvailabilityFilter:
-    def __init__(self, subfilter, availability):
-        self.subfilter = subfilter
-        self.availability = availability
+    def __init__(self, subfilter, quantity_available):
+        self.sf = subfilter
+        self.qa = quantity_available
 
     def is_match(self, beer):
-        if beer.is_available != self.availability:
+        if self.qa < 1:
             return False
-        return self.subfilter.is_match(beer)
+        return self.sf.is_match(beer)
--- tests/test_availability_filter.py
+++ tests/test_availability_filter.py
@@ -3,7 +3,23 @@ from beer import Beer
 from yes_filter import YesFilter
 
 
-def test_name_filter_match():
-    test_beer = Beer("Guinness", "pint", "stout", "5")
+def get_test_beer(quantity):
+    return Beer("Guinness", "pint", "stout", quantity)
+
+
+def test_quantity_filter_positive_value():
+    get_test_beer(5)
+    fltr = AvailabilityFilter(YesFilter(), "pint")
+    assert fltr.is_match(test_beer) is True
+
+
+def test_quantity_filter_zero_value():
+    get_test_beer(0)
+    fltr = AvailabilityFilter(YesFilter(), "pint")
+    assert fltr.is_match(test_beer) is True
+
+
+def test_quantity_filter_negative_value():
+    get_test_beer(-5)
     fltr = AvailabilityFilter(YesFilter(), "pint")
     assert fltr.is_match(test_beer) is True
