cc3f872801c307d2fdf8a6b79ccbbccdfe6a7670
Mario <stmos01@moravian.edu>
2018-03-23 14:58:33 -0400

    makes beer.py compliant with pylint

--- src/beer.py
+++ src/beer.py
@@ -1,6 +1,21 @@
+'''
+
+No libraries used
+
+'''
 class Beer:
+    '''
+
+    Beer Object
+
+    '''
 
     def __init__(self, name, size, style, price, quantity):
+        '''
+
+        initialize the beer object and its attributes
+
+        '''
         self.name = name
         self.size = size
         self.style = style
@@ -8,24 +23,51 @@ class Beer:
         self.quantity = quantity
 
     def get_name(self):
+        '''
+
+        Returns: name of the beer object
+
+        '''
         return self.name
 
     def get_size(self):
+        '''
+
+        Returns: size of the beer object
+
+        '''
         return self.size
 
     def get_style(self):
+        '''
+
+        Returns: style (aka type) of the beer object
+
+        '''
         return self.style
 
     def get_price(self):
+        '''
+
+        Returns: price of the beer object
+
+        '''
         return self.price
 
     def get_quantity(self):
+        '''
+
+        Returns: the quantity of the beer object
+
+        '''
         return self.quantity
 
     def is_available(self):
+        '''
+
+        Returns: whether the beer is available in stock
+
+        '''
         if int(self.get_quantity()) > 0:
             return True
-        else:
-            return False
-
-
+        return False
