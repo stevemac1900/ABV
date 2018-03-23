0eef9479440c7dc63b7dc45edbadb192e5516a90
Andrew O'Brien <obriena02@moravian.edu>
2018-03-23 14:40:56 -0400

    Make yes_filter compliant

--- src/yes_filter.py
+++ src/yes_filter.py
@@ -1,4 +1,9 @@
-class YesFilter:
+"""This module contains the base class for the database filters"""
+#pylint: disable=unused-argument
 
+class YesFilter:
+    "This is the base class of our filter"
+    @classmethod
     def is_match(self, beer):
+        "This method returns true for any beer"
         return True
