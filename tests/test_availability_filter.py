a774c693ebcff3527a0818948b9b0ce6f49269aa
Steve MacDonald <stsjm10@moravian.edu>
2018-03-23 12:51:16 -0400

    Correct and refactor testing

--- tests/test_availability_filter.py
+++ tests/test_availability_filter.py
@@ -4,19 +4,21 @@ from yes_filter import YesFilter
 
 
 def get_test_beer(quantity):
-    return Beer("Guinness", "pint", "stout", quantity)
+    return Beer("Guinness", "pint", "stout", 0, quantity)
+
+
+def get_test_filter(quantity):
+    return AvailabilityFilter(YesFilter(), quantity)
 
 
 def test_quantity_filter_positive_value():
-    fltr = AvailabilityFilter(YesFilter(), 5)
-    assert fltr.is_match(get_test_beer(5)) is True
+    assert get_test_filter(5).is_match(get_test_beer(5)) is True
 
 
 def test_quantity_filter_zero_value():
-    fltr = AvailabilityFilter(YesFilter(), 0)
-    assert fltr.is_match(get_test_beer(0)) is True
+    assert get_test_filter(0).is_match(get_test_beer(0)) is False
 
 
 def test_quantity_filter_negative_value():
-    fltr = AvailabilityFilter(YesFilter(), -5)
-    assert fltr.is_match(get_test_beer(-5)) is True
+    assert get_test_filter(-5).is_match(get_test_beer(-5)) is False
+
