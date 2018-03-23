8b5d6e46b9b443a0d23dbe850a5390b576c48b53
Mario <stmos01@moravian.edu>
2018-03-23 15:04:56 -0400

    Makes beer more compliant with Pylint

--- src/beer.py
+++ src/beer.py
@@ -1,21 +1,9 @@
-'''
-
-No libraries used
-
-'''
+''' No libraries used '''
 class Beer:
-    '''
-
-    Beer Object
-
-    '''
+    ''' Beer Object '''
 
     def __init__(self, name, size, style, price, quantity):
-        '''
-
-        initialize the beer object and its attributes
-
-        '''
+        ''' initialize the beer object and its attributes '''
         self.name = name
         self.size = size
         self.style = style
@@ -23,51 +11,27 @@ class Beer:
         self.quantity = quantity
 
     def get_name(self):
-        '''
-
-        Returns: name of the beer object
-
-        '''
+        ''' Returns: name of the beer object '''
         return self.name
 
     def get_size(self):
-        '''
-
-        Returns: size of the beer object
-
-        '''
+        ''' Returns: size of the beer object '''
         return self.size
 
     def get_style(self):
-        '''
-
-        Returns: style (aka type) of the beer object
-
-        '''
+        ''' Returns: style (aka type) of the beer object '''
         return self.style
 
     def get_price(self):
-        '''
-
-        Returns: price of the beer object
-
-        '''
+        ''' Returns: price of the beer object '''
         return self.price
 
     def get_quantity(self):
-        '''
-
-        Returns: the quantity of the beer object
-
-        '''
+        ''' Returns: the quantity of the beer object '''
         return self.quantity
 
     def is_available(self):
-        '''
-
-        Returns: whether the beer is available in stock
-
-        '''
+        ''' Returns: whether the beer is available in stock '''
         if int(self.get_quantity()) > 0:
             return True
         return False
