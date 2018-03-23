049c311b70fde432823a3093a27d9a1fdfe69770
Andrew O'Brien <obriena02@moravian.edu>
2018-03-23 14:55:02 -0400

    Move disable message

--- src/yes_filter.py
+++ src/yes_filter.py
@@ -1,9 +1,10 @@
 """This module contains the base class for the database filters"""
-#pylint: disable=unused-argument
 
 class YesFilter:
     "This is the base class of our filter"
+
+    # pylint: disable=unused-argument
     @classmethod
-    def is_match(self, beer):
+    def is_match(cls, beer):
         "This method returns true for any beer"
         return True
