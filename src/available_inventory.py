65c43c9b7d2e89fe978453387fcd98474ecf4038
Jose Meono <jose.a.meono@gmail.com>
2018-03-27 21:22:06 -0400

    Makes available inventory module complian with pylint

--- src/available_inventory.py
+++ src/available_inventory.py
@@ -1,12 +1,19 @@
+"""
+ This module is for the available inventory
+"""
 
-def make_subset(item):
-    return {key: item[key] for key in ['name', 'size', 'category', 'price', 'quantity', ]}
+
+def keep_requested_attributes(beer):
+    return {key: beer[key] for key in ['name', 'size', 'category', 'price', 'quantity', ]}
 
 
 class InventoryQueries:
-
+    """
+    This class initiates a Query of Inventory
+    """
     def __init__(self, inventory):
         self.inventory = inventory
 
     def get_available_inventory(self):
-        return [make_subset(item) for item in self.inventory.get_historic_inventory() if item['quantity'] > 0]
+        return [keep_requested_attributes(beer) for beer in self.inventory.get_historic_inventory()
+                if beer['quantity'] > 0]
