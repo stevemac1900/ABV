716436de30c843df842cad3f4e7f1a5a56d0501f
Jose Meono <jose.a.meono@gmail.com>
2018-03-27 21:16:57 -0400

    Makes filter ds module complaint with pylint

---
+++ src/filter_ds.py
@@ -0,0 +1,10 @@
+"""This module is for the Filter Data Structure.
+"""
+class FilterDS:
+    """This class initiates a Filter Data structure
+    """
+    def __init__(self, size=None, style=None, name=None, availability=None):
+        self.size = size
+        self.style = style
+        self.name = name
+        self.availability = availability
