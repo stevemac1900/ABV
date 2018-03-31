49ab4850ed4499d6a2b115a0746616405ff6395d
Andrew O'Brien <obriena02@moravian.edu>
2018-03-31 16:11:19 -0400

    Make test_filter_ds pylint compliant

--- tests/test_filter_ds.py
+++ tests/test_filter_ds.py
@@ -1,21 +1,22 @@
+"""This file tests the filter data structure"""
 from FilterDS import FilterDS
 
 def test_filter_with_no_filters():
-    filter = FilterDS()
-    assert None == filter.size
-    assert None == filter.style
+    ds_filter = FilterDS()
+    assert ds_filter.size is None
+    assert ds_filter.style is None
 
 def test_filter_with_package_filter():
-    filter = FilterDS(size='keg')
-    assert 'keg' == filter.size
-    assert None == filter.style
+    ds_filter = FilterDS(size='keg')
+    assert ds_filter.size == 'keg'
+    assert ds_filter.style is None
 
 def test_filter_with_style_filter():
-    filter = FilterDS(style='porter')
-    assert None == filter.size
-    assert 'porter' == filter.style
+    ds_filter = FilterDS(style='porter')
+    assert ds_filter.size is None
+    assert ds_filter.style == 'porter'
 
 def test_filter_with_both_filters():
-    filter = FilterDS(size='keg', style='porter')
-    assert 'keg' == filter.size
-    assert 'porter' == filter.style
+    ds_filter = FilterDS(size='keg', style='porter')
+    assert ds_filter.size == 'keg'
+    assert ds_filter.style == 'porter'
