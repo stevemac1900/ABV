24f7e07939de103536cd8b785d5a4cf4e5185c8b
Steve MacDonald <stsjm10@moravian.edu>
2018-03-31 23:35:23 -0400

    Makes filter check a requested quantity against available quantity

--- tests/test_availability_filter.py
+++ tests/test_availability_filter.py
@@ -3,21 +3,29 @@ from beer import Beer
 from yes_filter import YesFilter
 
 
-def make_test_beer(quantity):
-    return Beer("Guinness", "pint", "stout", 0, quantity)
+def make_test_beer(available):
+    return Beer("Guinness", "pint", "stout", 0, available)
 
 
-def get_test_filter(quantity):
-    return AvailabilityFilter(YesFilter(), quantity)
+def get_test_filter(quantity_requested):
+    return AvailabilityFilter(YesFilter(), quantity_requested)
 
 
-def test_quantity_filter_positive_value():
+def test_quantity_filter_request_five_five_available():
     assert get_test_filter(5).is_match(make_test_beer(5)) is True
 
 
-def test_quantity_filter_zero_value():
-    assert get_test_filter(0).is_match(make_test_beer(0)) is False
+def test_quantity_filter_request_five_less_available():
+    assert get_test_filter(5).is_match(make_test_beer(4)) is False
 
 
-def test_quantity_filter_negative_value():
-    assert get_test_filter(-5).is_match(make_test_beer(-5)) is False
+def test_quantity_filter_request_five_zero_available():
+    assert get_test_filter(5).is_match(make_test_beer(0)) is False
+
+
+def test_quantity_filter_request_five_six_available():
+    assert get_test_filter(5).is_match(make_test_beer(6)) is True
+
+
+def test_quantity_filter_request_five_negative_quantity_available():
+    assert get_test_filter(5).is_match(make_test_beer(-5)) is False
