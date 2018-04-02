9fbc077b4fb6748b0c40e66d56a9b460cae07a52
Mario <stmos01@moravian.edu>
2018-04-02 18:51:30 -0400

    Removed comments on getter functions

--- src/beer.py
+++ src/beer.py
@@ -1,6 +1,6 @@
 ''' File containing class for Beer object '''
 class Beer:
-    
+
     def __init__(self, name, size, style, price, quantity):
         ''' initialize the beer object and its attributes '''
         self.name = name
@@ -10,23 +10,18 @@ class Beer:
         self.quantity = quantity
 
     def get_name(self):
-        ''' Returns: name of the beer object '''
         return self.name
 
     def get_size(self):
-        ''' Returns: size of the beer object '''
         return self.size
 
     def get_style(self):
-        ''' Returns: style (aka type) of the beer object '''
         return self.style
 
     def get_price(self):
-        ''' Returns: price of the beer object '''
         return self.price
 
     def get_quantity(self):
-        ''' Returns: the quantity of the beer object '''
         return self.quantity
 
     def is_available(self):
