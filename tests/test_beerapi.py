3e67ba250fc0ea6d31fd3e6c109b14c768e5b53e
Jose Meono <jose.a.meono@gmail.com>
2018-04-02 17:52:26 -0400

    Make beerapi test more compliant with pylint

--- tests/test_beerapi.py
+++ tests/test_beerapi.py
@@ -1,28 +1,30 @@
+"""
+    This test is for the purpose of testing beerapi module with pytest.fixture and assuring requests works
+"""
 import pytest
 import requests
 import requests_mock
 import src.beerapi as beerapi
 
 
-@pytest.fixture(params= [requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError])
+@pytest.fixture(params=[requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError])
 def expected_exception_on_read(request):
     with requests_mock.Mocker() as session:
         session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', exc=request.param)
         yield request.param
 
 
-def test_connection_exceptions_on_read(expected_exception_on_read):
+def test_connection_exceptions_on_read():
     assert beerapi.get_inventory() is ""
 
 
-@pytest.fixture()
-def expected_exception_on_write():
+@pytest.fixture(params=[OSError])
+def expected_exception_on_write(request):
     with requests_mock.Mocker() as session:
-        session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', exc=OSError)
-        yield
+        session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', exc=request.param)
+        yield request.param
 
 
 def test_exception_on_write(expected_exception_on_write):
-    with pytest.raises(OSError):
-        beerapi.write_beer_inventory();
-
+    with pytest.raises(expected_exception_on_write):
+        beerapi.write_beer_inventory()
