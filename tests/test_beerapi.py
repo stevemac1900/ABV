a24f4ee35b69d28eaff71a6fcb7bbb56c7694717
Jose Meono <jose.a.meono@gmail.com>
2018-04-03 23:03:08 -0400

    Make test beerapi even more compliant with pylint

--- tests/test_beerapi.py
+++ tests/test_beerapi.py
@@ -1,27 +1,30 @@
 """
-    This test is for the purpose of testing beerapi module with pytest.fixture and assuring requests works
+    This test is for the purpose of testing beerapi module with pytest.fixture
+    and assuring requests works
 """
 import pytest
 import requests
 import requests_mock
 import src.beerapi as beerapi
 
+TANCZOS_INVENTORY = 'http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv'
+
 
 @pytest.fixture(params=[requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError])
 def expected_exception_on_read(request):
     with requests_mock.Mocker() as session:
-        session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', exc=request.param)
+        session.get(TANCZOS_INVENTORY, exc=request.param)
         yield request.param
 
 
-def test_connection_exceptions_on_read():
+def test_connection_exceptions_on_read(expected_exception_on_read):
     assert beerapi.get_inventory() is ""
 
 
 @pytest.fixture(params=[OSError])
 def expected_exception_on_write(request):
     with requests_mock.Mocker() as session:
-        session.get('http://www.tanczos.com/tanczos.com/beerinventory/webexport.csv', exc=request.param)
+        session.get(TANCZOS_INVENTORY, exc=request.param)
         yield request.param
 
 
