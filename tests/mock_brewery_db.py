a8baf1770446d4ff2539e568f40f97a6daddc96f
Andrew O'Brien <obriena02@moravian.edu>
2018-04-06 15:22:37 -0400

    Added new mock objects

--- tests/mock_brewery_db.py
+++ tests/mock_brewery_db.py
@@ -1,5 +1,15 @@
 class MockBreweryDB:
+    def get_beer_style(self, beer_name):
+        if beer_name == "Guiness":
+            return "stout"
+        assert False
+
+class MockBreweryDBAlwaysStout:
     def get_beer_style(beer_name):
-        if beer_name is None:
-         assert False
-        return "Stout"
+        return "stout"
+
+class MockBreweryDBAlwaysError:
+    count = 0
+    def get_beer_style(cls, beer_name):
+        cls.count += 1
+        raise Exception('Error')
