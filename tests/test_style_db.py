561fb0f391b4bcfba905f16a14368a93ff295637
Andrew O'Brien <obriena02@moravian.edu>
2018-04-09 13:56:36 -0400

    Add tests for the style database

--- tests/test_style_db.py
+++ tests/test_style_db.py
@@ -1,38 +1,43 @@
-from tests.mock_brewery_db import MockBreweryDBAlwaysStout
+from tests.mock_brewery_db import MockBreweryDBStoutTracked
 from tests.mock_brewery_db import MockBreweryDBAlwaysError
-from tests.mock_styledb import StyleDB
-from tests.mock_brewery_db import MockBreweryDBAlwaysUnknown
-import pytest
+from tests.mock_brewery_db import MockBreweryDBUnknownTracked
+from abv.style_db import StyleDB
 
 
-#Tests that stout is correctly returned when cache has it
+
+#Tests that stout is correctly returned when cache has it. Also that database is never seached
 def test_cache_has_beer():
     styles_dict = {'Nitro': 'stout'}
-    styles_brewery_db = MockBreweryDBAlwaysStout()
-    styles = StyleDB(styles_dict,styles_brewery_db)
-    assert styles.getStyle('Nitro') == 'stout'
-
-#Tests if the database has the style and not the cache
-def test_db_has_style():
-    styles_dict = {}
-    styles_brewery_db = MockBreweryDBAlwaysStout()
+    styles_brewery_db = MockBreweryDBStoutTracked()
     styles = StyleDB(styles_dict, styles_brewery_db)
-    assert styles.getStyle('Guiness') == 'stout'
+    assert styles.get_style('Nitro') == 'stout'
+    assert styles.brew_db.count == 0
 
-#Tests that looks up once in database if cache doesnt have and then not again (now in cache)
-def test_searches_database_only_once():
+#Tests if the database has the style and not the cache. And then second search takes from cache.
+def test_db_has_style_and_then_cache():
     styles_dict = {}
-    styles_brewery_db = MockBreweryDBAlwaysUnknown()
+    styles_brewery_db = MockBreweryDBStoutTracked()
     styles = StyleDB(styles_dict, styles_brewery_db)
-    assert styles.getStyle("Duck Tails") == "Unknown"
-    assert styles.beer_dict["Duck Tails"] == "Unknown"
+    tested = styles.get_style('Guiness')
+    print(tested)
+    assert styles.get_style('Guiness') == 'stout'
+    assert styles.get_style('Guiness') == 'stout'
+    assert styles.brew_db.count == 1
+
+#Tests if unknown is first found by database and then from cache
+def test_unknown_from_db_then_cache():
+    styles_dict = {}
+    styles_brewery_db = MockBreweryDBUnknownTracked()
+    styles = StyleDB(styles_dict, styles_brewery_db)
+    assert styles.get_style('Duck Tails') == 'Unknown'
+    assert styles.get_style('Duck Tails') == 'Unknown'
+    assert styles.brew_db.count == 1
 
 #Tests that if query throws error once, that it throws it again by same input
 def test_two_against_db():
     style_dict = {}
     mocked_db = MockBreweryDBAlwaysError()
     styles = StyleDB(style_dict, mocked_db)
-    with pytest.raises(Exception):
-        assert styles.getStyle('Guiness') == "Unknown"
-        assert styles.getStyle('Guiness') == "Unknown"
+    assert styles.get_style('Guiness') == "Unknown"
+    assert styles.get_style('Guiness') == "Unknown"
     assert mocked_db.count == 2
