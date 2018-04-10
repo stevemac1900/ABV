ae7f69571116e87237f408650cae966fafdc2ee7
mtnesbitt <stmtn01@moravian.edu>
2018-04-10 14:28:13 -0400

    Have tests utilize actual url for brewerydb.

--- tests/test_brewerydb_queries.py
+++ tests/test_brewerydb_queries.py
@@ -16,34 +16,34 @@ from abv.brewerydb_queries import BreweryDBQueries
     # 404 -- bad endpoint
 
 
-url = 'http://api/search?key=1&q=Guinness'
-
+BREWERY_DB_URL = "http://api.brewerydb.com/v2/search?key=mock_key&q=Guinness" + "&type=beer"
+QUERIES = BreweryDBQueries()
 
 @pytest.fixture()
 def no_name():
     with requests_mock.Mocker() as session:
-        session.get(url, json={'status': 'success'})
+        session.get(BREWERY_DB_URL, json={'status': 'success'})
         yield
 
 
 @pytest.fixture()
 def no_style():
     with requests_mock.Mocker() as session:
-        session.get(url, json={'status': 'success', 'data': [{}]})
+        session.get(BREWERY_DB_URL, json={'status': 'success', 'data': [{}]})
         yield
 
 
 @pytest.fixture()
 def no_style_result():
     with requests_mock.Mocker() as session:
-        session.get(url, json={'status': 'success', 'data': [{'style': {'name': ''}}]})
+        session.get(BREWERY_DB_URL, json={'status': 'success', 'data': [{'style': {'name': ''}}]})
         yield
 
 
 @pytest.fixture()
 def single_result():
     with requests_mock.Mocker() as session:
-        session.get(url, json={'status': 'success', 'data': [{'style': {'name': 'Stout'}}]})
+        session.get(BREWERY_DB_URL, json={'status': 'success', 'data': [{'style': {'name': 'Stout'}}]})
         yield
 
 
@@ -51,36 +51,31 @@ def single_result():
 @pytest.fixture(params=[requests.exceptions.RequestException])
 def failed_request(request):
     with requests_mock.Mocker() as session:
-        session.get('http://api/search?key=1&q=Guinness', exc=request.param)
+        session.get('http://api.brewerydb.com/v2/search?key=mock_key&q=Guinness&type=beer', exc=request.param)
 
         yield request.param
 
 
 def test_no_name(no_name):
-    queries = BreweryDBQueries(API_KEY='1')
-    style = queries.get_beer_style('Guinness')
+    style = QUERIES.get_beer_style('Guinness')
     assert style == 'Unknown'
 
 
 def test_no_style(no_style):
-    queries = BreweryDBQueries(API_KEY='1')
-    style = queries.get_beer_style('Guinness')
+    style = QUERIES.get_beer_style('Guinness')
     assert style == 'Unknown'
 
 
 def test_no_style_result(no_style_result):
-    queries = BreweryDBQueries(API_KEY='1')
-    style = queries.get_beer_style('Guinness')
+    style = QUERIES.get_beer_style('Guinness')
     assert style == 'Unknown'
 
 
 def test_single_result(single_result):
-    queries = BreweryDBQueries(API_KEY='1')
-    style = queries.get_beer_style('Guinness')
+    style = QUERIES.get_beer_style('Guinness')
     assert style == 'Stout'
 
 
 def test_failed_request(failed_request):
-    queries = BreweryDBQueries(API_KEY='1')
-    style = queries.get_beer_style('Guinness')
+    style = QUERIES.get_beer_style('Guinness')
     assert style == ''
