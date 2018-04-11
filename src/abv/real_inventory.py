6e211f4fb4785d39ef4b8cfffd11b637afa95883
Jose Meono <jose.a.meono@gmail.com>
2018-04-11 13:04:33 -0400

    testing to make the real inventory

---
+++ src/abv/real_inventory.py
@@ -0,0 +1,19 @@
+from abv.style_db import StyleDB
+from abv.beer import Beer
+
+class Inventory:
+
+    def __init__(self):
+        self.inventory = []
+        inv_dict = {}
+        tanczos_inventory =  MostRecentFile()
+        style_db = StyleDB(inv_dict,)
+
+        for beer in tanczos_inventory:
+            style = style_db.get_style(beer.name)
+            full_beer = Beer(beer.name,beer.size,style,beer.price,beer.quantity)
+            self.inventory.append(full_beer)
+
+    def get_historic_inventory(self):
+        return self.inventory
+        
