f0344aadb1e1a3449d557fa7e4a0bf0d27df1f5f
Jose Meono <jose.a.meono@gmail.com>
2018-04-12 19:28:04 -0400

    renamed to current_inventory and refactored to correct location

--- src/abv/inventory_api/current_inventory.py
+++ src/abv/inventory_api/current_inventory.py
@@ -1,17 +1,17 @@
-from abv.style_db import StyleDB
-from abv.beer import Beer
+from abv.inventory_api.style_db import StyleDB
+from abv.inventory_api.beer import Beer
 
 
 class Inventory:
     def __init__(self):
         self.inventory = []
 
-        tanczos_inventory =  MostRecentFile()
+        tanczos_inventory = MostRecentFile()
         style_db = StyleDB()
 
         for beer in tanczos_inventory:
             style = style_db.get_style(beer.name)
-            full_beer = Beer(beer.name,beer.size,style,beer.price,beer.quantity)
+            full_beer = Beer(beer.name, beer.size, style, beer. price, beer.quantity)
             self.inventory.append(full_beer)
 
     def get_historic_inventory(self):
