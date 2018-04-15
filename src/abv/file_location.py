a5f58a0dc596caef2e547d290c08324366c31675
Andrew O'Brien <obriena02@moravian.edu>
2018-04-15 15:28:23 -0400

    Make changes John suggestion and improve comments

--- src/abv/file_location.py
+++ src/abv/file_location.py
@@ -1,12 +1,10 @@
 #pylint: disable=too-few-public-methods
-#pylint: disable=missing-docstring
+"""This class enables the location for the fetched files to be abstracted."""
 
 class FileLocation:
-    #This class allows us to abstract the location for our fetched files
 
     def __init__(self):
         self.current_location = "/beer_data"
 
     def get_file_location(self):
-        "Returns the current location where we want files to be stored"
         return self.current_location
