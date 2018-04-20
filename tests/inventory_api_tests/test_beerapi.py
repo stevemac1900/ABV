b311c127a8a2c43c01fdf734b0ce7c9f96fa959b
mtnesbitt <stmtn01@moravian.edu>
2018-04-20 13:57:55 -0400

    Add a test for the initialize_inventory function.

--- tests/inventory_api_tests/test_beerapi.py
+++ tests/inventory_api_tests/test_beerapi.py
@@ -2,21 +2,26 @@ from tests.inventory_api_tests.mock_inventory_queries import MockInventoryQuerie
 from abv.inventory_api.beer import Beer
 import abv.inventory_api.beerapi as beerapi
 from tests.inventory_api_tests.mock_inventory_queries_saving import MockInventoryQueriesSaving
+from abv.file_location import FileLocation
+
 beerapi.app.testing = True
 app = beerapi.app.test_client()
 
 pabst = Beer(name='Pabst', size='12/12oz', style='lager', quantity='1', price='20')
 guinness = Beer(name='Guinness', size='12/12oz', style='stout', quantity='1', price='30')
 
+
 def test_empty_results():
     beerapi.queries = MockInventoryQueries([])
     result = app.get('/current')
     assert result.data == b'[]'
 
+
 def test_single_result():
     beerapi.queries = MockInventoryQueries([pabst])
     result = app.get('/current')
     assert result.data == b'[{"name": "Pabst", "size": "12/12oz", "style": "lager", "quantity": "1", "price": "20"}]'
+
 
 def test_two_results():
     beerapi.queries = MockInventoryQueries([pabst, guinness])
@@ -24,10 +29,6 @@ def test_two_results():
     assert result.data == b'[{"name": "Pabst", "size": "12/12oz", "style": "lager", "quantity": "1", "price": "20"}, ' \
                           b'{"name": "Guinness", "size": "12/12oz", "style": "stout", "quantity": "1", "price": "30"}]'
 
-# def test_filtered_results():
-#     beerapi.queries = MockInventoryQueries([pabst, guinness])
-#     result = app.get('/current?name=Pabst')
-#     assert result.data == b'[{"name": "Pabst", "size": "12/12oz", "style": "lager", "quantity": "1", "price": "20"}]'
 
 def test_no_filter():
     saver = MockInventoryQueriesSaving()
@@ -36,12 +37,14 @@ def test_no_filter():
     assert saver.filter.name is None
     assert saver.filter.style is None
 
+
 def test_name_filter():
     saver = MockInventoryQueriesSaving()
     beerapi.queries = saver
     app.get('/current?name=Guinness')
     assert beerapi.queries.filter.name == 'Guinness'
     assert saver.filter.style is None
+
 
 def test_availability_filter():
     saver = MockInventoryQueriesSaving()
@@ -50,6 +53,7 @@ def test_availability_filter():
     assert saver.filter.name == 'Guinness'
     assert saver.filter.availability == '2'
     assert saver.filter.style == None
+
 
 def test_style_filter():
     saver = MockInventoryQueriesSaving()
@@ -60,6 +64,7 @@ def test_style_filter():
     assert saver.filter.style == 'stout'
     assert saver.filter.size is None
 
+
 def test_size_filter():
     saver = MockInventoryQueriesSaving()
     beerapi.queries = saver
@@ -69,11 +74,13 @@ def test_size_filter():
     assert saver.filter.style == 'stout'
     assert saver.filter.size == '12/12OZ'
 
+
 def test_repeat_params():
     saver = MockInventoryQueriesSaving()
     beerapi.queries = saver
     app.get('/current?name=Guinness&name=Pabst')
     assert saver.filter.name == 'Guinness'
+
 
 def test_bad_params():
     saver = MockInventoryQueriesSaving()
@@ -81,8 +88,15 @@ def test_bad_params():
     result = app.get('/current?foo=bar')
     assert result.status_code == 400
 
+
 def test_bad_and_good_params():
     saver = MockInventoryQueriesSaving()
     beerapi.queries = saver
     result = app.get('/current?name=Guinness&foo=bar')
     assert result.status_code == 400
+
+
+def test_api_initialization():
+    FileLocation.save_location = 'tests/sample_csv_files'
+    beerapi.initialize_inventory()
+    assert beerapi.queries.inventory.get_historic_inventory()[0].name == 'BREW WORKS HOP EXPLOSION'
