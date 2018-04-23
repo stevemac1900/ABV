34d9f83550c11e6be89bfdab497169d2b4882efd
Andrew O'Brien <obriena02@moravian.edu>
2018-04-23 11:54:38 -0400

    Implemented testing for the twilio flask

---
+++ tests/twilio_flask_tests/test_twilio_flask.py
@@ -0,0 +1,56 @@
+import re
+from pytest import fixture
+import requests_mock
+import src.abv.twilio_api.twilio_flask as twilio_flask
+
+
+
+
+@fixture()
+def app():
+    twilio_flask.APP.testing = True
+    inner_app = twilio_flask.APP.test_client()
+    return inner_app
+
+
+# pylint: disable=redefined-outer-name
+def test_bad_status_code(app):
+    with requests_mock.Mocker() as m:
+        m.register_uri('GET', re.compile('beerapi'), text='[]', status_code=404)
+        result = app.get('/', data={'Body':'stout'})
+        assert b'Sorry, I cannot handle your request. Please try again later!' in result.data
+
+
+# pylint: disable=redefined-outer-name
+def test_valid_twilio_format(app):
+    with requests_mock.Mocker() as m:
+        m.register_uri('GET', re.compile('beerapi'), text='[{"name":"foo"}, {"name":"foo"}]')
+        result = app.get('/', data={'Body': 'stout'})
+        assert result.status_code == 200
+        assert result.data.startswith(b'<?xml')
+
+
+# pylint: disable=redefined-outer-name
+def test_no_results(app):
+    with requests_mock.Mocker() as m:
+        m.register_uri('GET', re.compile('beerapi'), text='[]')
+        result = app.get('/', data={'Body':'stout'})
+        assert b'Sorry, no results for stout' in result.data
+
+# pylint: disable=redefined-outer-name
+def test_one_result(app):
+    with requests_mock.Mocker() as m:
+        m.register_uri('GET', re.compile('beerapi'), text='[{"name":"foo"}]')
+        result = app.get('/', data={'Body': 'porter'})
+        assert b'There is 1 beer with the style porter' in result.data
+
+# pylint: disable=redefined-outer-name
+def test_tw0_result(app):
+    with requests_mock.Mocker() as m:
+        m.register_uri('GET', re.compile('beerapi'), text='[{"name":"foo"}, {"name":"bar"}]')
+        result = app.get('/', data={'Body': 'IPA'})
+        assert b'There are 2 beers with the style IPA' in result.data
+
+
+def test_count_beers():
+    assert twilio_flask.count_beers('[{}, {}]') == 2
