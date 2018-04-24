4a702c03d7b8baa2bd0212127c679bbd89ccf32c
Andrew O'Brien <obriena02@moravian.edu>
2018-04-24 16:05:39 -0400

    Add the style cache file as well as associated tests and test files

---
+++ tests/inventory_api_tests/test_style_cache.py
@@ -0,0 +1,23 @@
+import tempfile
+from abv.inventory_api.style_cache import StyleCache
+
+def test_blank_file():
+    test_cache = StyleCache("beer_data/empty.csv")
+    print(test_cache.cache_dict)
+    assert test_cache.cache_dict == {}
+
+def test_can_read_and_lookup():
+    test_cache = StyleCache("tests/sample_csv_files/beer_and_style.csv")
+    assert test_cache.look_up("Guinness") == "Stout"
+    assert test_cache.look_up("Pabst Blue Ribbon") == "Lager"
+    assert test_cache.look_up("Fat Tire") == "Belgian Ale"
+
+def test_invalid_beer_lookup():
+    test_cache = StyleCache("tests/sample_csv_files/beer_and_style.csv")
+    assert test_cache.look_up("Rude Elf Reserve") is None
+
+def test_add_beer():
+    temp_file = tempfile.NamedTemporaryFile(dir='tests/sample_csv_files', prefix='b')
+    test_cache = StyleCache(temp_file.name)
+    test_cache.add("Rude Elf Reserve", "Belgian")
+    assert test_cache.look_up("Rude Elf Reserve") == "Belgian"
