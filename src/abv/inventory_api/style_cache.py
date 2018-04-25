16ec70752f642b1a9e3696e8dbd71d7c97b5d402
Andrew O'Brien <obriena02@moravian.edu>
2018-04-25 13:59:20 -0400

    Remove redundent style_cache comments and refactor the for loop

--- src/abv/inventory_api/style_cache.py
+++ src/abv/inventory_api/style_cache.py
@@ -1,30 +1,23 @@
-
-"""This class creates a style cache that holds recently searched for beers."""
+# pylint: disable=missing-docstring
 class StyleCache:
 
-    """Cache constructor."""
     def __init__(self, filename):
         self.filename = filename
         self.cache_dict = {}
 
         with open(self.filename, 'r') as infile:
-            i = 0
-            for line in infile:
-                if i == 0:
-                    i += 1
-                    continue
+            lines = infile.readlines()[1:]
+            for line in lines:
                 line = line.strip()
                 beer_data = line.split(',')
                 self.cache_dict[beer_data[0]] = beer_data[1]
 
     def look_up(self, beer_name):
-        """Beer look up method """
         if beer_name in self.cache_dict:
             return str(self.cache_dict[beer_name])
         return None
 
     def add(self, beer, style):
-        """Adds the searched beer to the file"""
         self.cache_dict[beer] = style
         with open(self.filename, "a") as myfile:
             myfile.write(beer + ',' + style + '\n')
